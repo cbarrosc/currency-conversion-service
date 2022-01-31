9e2cacead218fb837264e664900eca59c2f1112d
cbarros <camilo.barros@globant.com>
2022-01-31 12:53:37 -0300

    implemented feign client

--- src/main/java/com/globant/microservice/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/globant/microservice/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,6 @@
 package com.globant.microservice.currencyconversionservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,10 +9,12 @@ import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeProxy proxy;
 
     @GetMapping("currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
@@ -30,6 +33,20 @@ public class CurrencyConversionController {
                                                         currencyConversion.getConversionMultiple(),
                                                         quantity.multiply(currencyConversion.getConversionMultiple()),
                 currencyConversion.getEnvironment());
+
+    }
+
+    @GetMapping("currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from,to);
+        return new CurrencyConversion(currencyConversion.getId(),
+                from,
+                to,
+                quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " " + "feign");
 
     }
 
