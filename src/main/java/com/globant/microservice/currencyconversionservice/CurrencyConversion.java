2822ed122c7a3e05b4627d43b22eb844796be9fd
cbarros <camilo.barros@globant.com>
2022-01-31 12:36:50 -0300

    Initial commit

---
+++ src/main/java/com/globant/microservice/currencyconversionservice/CurrencyConversion.java
@@ -0,0 +1,25 @@
+package com.globant.microservice.currencyconversionservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CurrencyConversion {
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+
+
+}
